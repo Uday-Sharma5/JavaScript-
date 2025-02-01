614315e24903e725c59e2df179db219d36190be3
udayweyyin <uday.weyyin@gmail.com>
2025-02-01 16:54:59 +0530

    Added new JavaScript examples

---
+++ eg2.js
@@ -0,0 +1,5 @@
+var x;
+x=10;
+console.log(x);
+x="GOD IS GREAT";
+console.log(x);
---
+++ eg3.js
@@ -0,0 +1,8 @@
+var x;
+x=true;
+console.log(x);
+x=2.23;
+console.log(x);
+var y;
+console.log(y);
+console.log(z);
