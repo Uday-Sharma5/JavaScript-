9a341a2f513b0263520753b5e9d0ebee8da52c1b
Uday Sharma <udaysharma.2310@gmail.com>
2025-02-04 19:13:45 +0530

    Added new JavaScript Example

---
+++ eg10.js
@@ -0,0 +1,23 @@
+var x="10";
+if(x==10)
+{
+console.log("GREAT");
+}
+else
+{
+console.log("COOL");
+}
+
+/*EVEN THOUGH WE ARE ASSIGING THE STRING TO X BUT THE NUMBER IS IN THE STRING WERE SAME AS THE NUMBER IN CONDITION IT EVALUATE TRUE.
+THEY DO NOT CHECK WHATS THE DATA TYPE BEACUSE OF ==. */
+
+// === uses to check the datatype.
+var m="100";
+if(m===100)
+{
+console.log("GOOD");
+}
+else
+{
+console.log("FALSE");
+}
