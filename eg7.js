c4f63fc4a20d20031cc0124df6d524c819f0fae4
Uday Sharma <udaysharma.2310@gmail.com>
2025-02-04 20:13:01 +0530

    Update eg7.js

--- eg7.js
+++ eg7.js
@@ -1,12 +1,14 @@
 var m=10;
 k=i=j=m;
+//Now, all variables have the value 10.
 console.log(m);
 console.log(j);
 console.log(i);
 console.log(k);
+//k++ increases k by 1.
 k++;
-m=m+1;
-++i;
+m=m+1; //m = m + 1 increases m by 1.
+++i; //++i increases i by 1.
 console.log("-------------------");
 console.log(k);
 console.log(m);
