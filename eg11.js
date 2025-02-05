848bf160098e250f6e4f70c95df5b9b6d990117c
Uday Sharma <udaysharma.2310@gmail.com>
2025-02-05 16:29:38 +0530

    Added new JavaScript examples

---
+++ eg11.js
@@ -0,0 +1,13 @@
+// string comparison:- its not about how many characters in a string, its about which character is heavier in weight.
+
+var x,y;
+x="Bombay";
+y="Mumbai";
+if(x<y)
+{
+console.log("Weight of Bombay is less than Ujjain");
+}
+else
+{
+console.log("Weight of Ujjain is greater than Bombay");
+}
---
+++ eg12.js
@@ -0,0 +1,10 @@
+/*if in between + has both side 2 operants are numeric, so it performs maths. 
+But one side operants is a string type then it concatinate.*/
+//It only performs in + operator.
+
+var x;
+x="Bombay";
+var y;
+y="Bombay"+20;
+console.log(y);
+
