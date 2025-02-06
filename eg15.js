a7aac1932c1b12a2dd1d4a9f89941164201bdf6e
Uday Sharma <udaysharma.2310@gmail.com>
2025-02-06 17:00:28 +0530

    Added New Example

---
+++ eg15.js
@@ -0,0 +1,24 @@
+//IF DECIMAL VALUE IS LIKE 2.45 IS CONSIDER AS TRUE. 
+
+var h=2.45;
+if(h) console.log("True");
+else console.log('False');
+
+//0.0 IS CONSIDER AS FALSE.
+var z=0.0;
+if(z) console.log("T");
+else console.log("F");
+
+
+console.log("----------------------")
+// IF WE ASSIGN x TO null WHICH MEANS EMPTY. AND IF CONDITION HAS ONLY x THEN IT WILL BE FALSE.
+
+var x=null;
+if(x)
+{
+console.log("True");
+}
+else
+{
+console.log("False");
+}
