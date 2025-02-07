e7b1303d2f62033ab18c67cfe0485365af21ef61
Uday Sharma <udaysharma.2310@gmail.com>
2025-02-07 18:49:38 +0530

    New JS Examples

---
+++ eg22.js
@@ -0,0 +1,5 @@
+//SAME THING HAPPENS WITH % module OPERATOR.
+
+var x="10";
+var y=3;
+console.log(x%y);
---
+++ eg24.js
@@ -0,0 +1,12 @@
+var x="BOMBAY";
+var y=20;
+var z=x/y;
+var k=x%y;
+var i=x-y;
+var j=x*y;
+console.log(z);
+console.log(k);
+console.log(i);
+console.log(j);
+// STRING MULTIPLYED, SUBTRACT, DIVIDE BY ANYTHING IS GOING TO TURN OUT TO BE A NaN.
+//ONLY THING THAT IS GOING TO WORK IS ADDTION BEACUSE THEY CONCAITNATE.
