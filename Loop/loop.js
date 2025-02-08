6776be9ef189c64a46dcc6aabe23f4f9f3c373f7
Uday Sharma <udaysharma.2310@gmail.com>
2025-02-08 16:04:04 +0530

    Added JavaScript loop example

--- Loop/loop.js
+++ Loop/loop.js
@@ -1,1 +1,35 @@
+// A loop is a programming construct that allows you to execute a block of code multiple times. It helps automate repetitive tasks.
+// There are several types of loop: while, do while, for.
 
+//while loop
+var x=1;
+while(x<=10)
+{
+console.log(x);
+x++;
+}
+
+//for loop
+for(j=1;j<=5;j++)
+{
+console.log(j);
+}
+
+//do while
+i=1;
+do
+{
+console.log(i);
+i++;
+}while(i<=7)
+
+
+// WE CANNOT THINK THAT IN for LOOP THE FIRST STATEMENT IS ALWAYS BE A ASSIGNMENT AND LAST STATEMENT IS ALWAYS A INCREMENT OR DECRIMENT.
+
+var q;
+q=1;
+for(console.log("GREAT");q<=5;console.log("COOL UDAY"))
+{
+console.log(q);
+q++;
+}
