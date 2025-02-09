2d5256ceed90a6a569c07ffc2b5e01716dfeaf00
Uday Sharma <udaysharma.2310@gmail.com>
2025-02-09 15:29:30 +0530

    Added Function Example

---
+++ Function/function.js
@@ -0,0 +1,11 @@
+//a function is a block of reusable code designed to perform a specific task. Functions help in organizing code, improving reusability, and making programs more modular.
+// A FUNCTION IS A KEYWORD.
+
+function add(x,y)
+{
+var z=x+y;
+console.log("Total is"+z);
+}
+
+add(10,20);
+add(200,300);
