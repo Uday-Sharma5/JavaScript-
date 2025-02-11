d555b9b0ca223f681b02d60f61579369aa0e1fb0
Uday Sharma <udaysharma.2310@gmail.com>
2025-02-11 18:56:27 +0530

    Added new Examples

---
+++ Function/f11.js
@@ -0,0 +1,13 @@
+function add(a,b,c,d)
+{
+z=a+b+c+d;
+console.log(z);
+}
+
+add();   // IF WE CANNOT PASS ANYTHING THEN ALSO THIS PROGRAMS RUN
+add(10);  // output will be NaN
+add(10,10); // output will be NaN
+add(10,10,10); // output will be NaN
+add(10,10,10,10); // output will be NaN
+
+// Solution of this problem is in f12.js
