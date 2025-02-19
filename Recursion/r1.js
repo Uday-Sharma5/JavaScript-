af06613a3298bc9111c5054040a4546c122ed87c
Uday Sharma <udaysharma.2310@gmail.com>
2025-02-19 17:20:02 +0530

    Added New Examples

--- Recursion/r1.js
+++ Recursion/r1.js
@@ -1,1 +1,8 @@
-
+function sam(p)
+{
+if(p==1) return 1;
+var n=sam(p-1)*p;
+return n;
+}
+x=sam(5);
+console.log(x);
---
+++ Recursion/recursion.js
@@ -0,0 +1,7 @@
+function sam(k)
+{
+if(k==4) return;
+sam(k+1);
+console.log(k);
+}
+sam(1);
