d762b6cda9f71f28f9bb15e86400b479ee42998d
Uday Sharma <udaysharma.2310@gmail.com>
2025-02-20 22:58:08 +0530

    Added New Array Examples.

---
+++ Array/a18.js
@@ -0,0 +1,6 @@
+var x=[10,20,30];
+console.log(x);
+console.log(x.length);
+delete x[1]; // THE ELEMENT OF THE ARRAY IS NOT DELETE ONLY THE DATA IS DELETED.
+console.log(x);
+console.log(x.length);// AND THE LENGTH IS ALSO SAME
