324b1906c54e180984ff7ae81346837bafd7df3e
Uday Sharma <udaysharma.2310@gmail.com>
2025-02-21 18:43:34 +0530

    Added New Examples.

---
+++ Methods/concat.js
@@ -0,0 +1,11 @@
+var x=[10,20,30,40,50];
+var y=[60,70,80,90,100];
+console.log(x);
+console.log(y);
+
+var z=x.concat(y);//The concat() method joins two or more strings.
+
+console.log(x); //The concat() method does not change the existing strings.
+console.log(y); //The concat() method does not change the existing strings.
+
+console.log(z);//The concat() method returns a new string.
---
+++ Methods/copyWithin.js
@@ -0,0 +1,3 @@
+var x=[10,20,7897,46,4,98,4564];
+x.copyWithin(2,5); //5th INDEX TO END OF THE ARRAY PRINTS TO THE 2nd INDEX OF ARRAY.
+console.log(x);
