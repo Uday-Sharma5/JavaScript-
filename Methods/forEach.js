473b095a7070ed2bee1606e16e24c124e4ee9708
Uday Sharma <udaysharma.2310@gmail.com>
2025-05-25 22:38:42 +0530

    Added new method example.

---
+++ Methods/forEach.js
@@ -0,0 +1,9 @@
+function sam(e,i,a)
+{
+console.log(e);
+console.log(i);
+console.log(a);
+}
+
+var x=[10,20,30,40,50,60,70,80,90,100];
+var y=x.forEach(sam); // forEach ACCESING ALL THE ELEMENT IN AN ARRAY. EVERY ELEMENT OF ARRAY SAM FUNCTION GOT CALLED AND ELEMENTS WERE PASSED ONE BY ONE. 
