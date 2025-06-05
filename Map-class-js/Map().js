d7761d698d65686574987ec2c1ec3ba69dbd8038
Uday Sharma <udaysharma.2310@gmail.com>
2025-06-05 20:06:51 +0530

    Added New JavaScript Examples.

---
+++ Map-class-js/Map().js
@@ -0,0 +1,9 @@
+var v=new Map(); // A MAP IS A BUILD IN JAVASCRIPT OBJECT THAT HOLDS KEY VALUE PAIRS
+v.set(101,"NEERAJ"); //IN HERE WE CAN SETTING THE KEY AND VALUE TO v
+v.set(102,"HARI");
+v.set(103,"TOM");
+v.set(104,"PRAFULL");
+v.set(105,"HASSAN")
+console.log(v); // ALL THE KEY-VALUE IS PRINTED
+a=v.get(105); // FOR FINDING VALUE 
+console.log(a);
--- Map-class-js/m1.js
+++ Map-class-js/m1.js
@@ -1,1 +1,10 @@
-
+var v=new Map();
+v.set(101,"Rakesh");
+v.set(102,"ULLU");
+v.set(103,"RAJUU");
+v.set(104,"Chota Bheem");
+v.set(105,"OILIVA");
+console.log(v);
+console.log(v.has(105)); // Has is used to check if value has or not(TRUE)
+console.log(v.has(104));//True
+console.log(v.has(1000));//False beacuse it was not found in v Map;
