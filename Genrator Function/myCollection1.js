8997d0453bcc4a18a69586960c8f422d2e4f9aff
Uday Sharma <udaysharma.2310@gmail.com>
2025-06-08 13:50:55 +0530

    Added New JavaScript Examples

---
+++ Genrator Function/myCollection1.js
@@ -0,0 +1,28 @@
+function myCollection()
+{
+var collection=[];
+this.add=function(num)
+{
+collection.push(num);
+}
+this.iterator=function*(){
+var index=0;
+while(index<collection.length)
+{
+yield collection[index];
+index++;
+}
+}
+}
+
+var m=new myCollection();
+m.add(50);
+m.add(100);
+m.add(150);
+m.add(200);
+m.add(500);
+var k=m.iterator();
+for(var i of e)
+{
+console.log(i);
+}
