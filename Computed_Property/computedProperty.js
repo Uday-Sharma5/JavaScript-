d971bcfba58dc9bbeb60eea0d5fd6095d9b9f673
Uday Sharma <udaysharma.2310@gmail.com>
2025-06-09 17:04:29 +0530

    Added New JavaScript Example.

--- Computed_Property/computedProperty.js
+++ Computed_Property/computedProperty.js
@@ -1,1 +1,6 @@
-
+var e="Cool"; 
+var m={
+[e]:"GREAT"  // If a variable is written inside square brackets while defining an object, it is used as the property name of the object, not as a variable itself — only the value of that variable is assigned.
+};
+console.log(m); 
+console.log(m.Cool);
---
+++ Computed_Property/computedProperty1.js
@@ -0,0 +1,7 @@
+var m="Cool";
+function something()
+{
+this.Cool="Great";
+}
+var k=new something();
+console.log(k.Cool);
