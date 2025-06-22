5cf8be2f1e92feca766cf84cbdc42f143f60ba9a
Uday <uday.weyyin@gmail.com>
2025-06-22 20:29:21 +0530

    move files

---
+++ operator/instanceOf.js
@@ -0,0 +1,10 @@
+function abcd()
+{
+this.something="God is great";
+}
+var a={
+something:"God is great"
+};
+var v=new abcd();
+console.log(v instanceof Object);
+console.log(a instanceof Object);
