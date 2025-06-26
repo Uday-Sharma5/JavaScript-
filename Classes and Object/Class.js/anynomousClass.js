2ead2c303d9229c7952c54e67c84d3c50f0b20aa
Uday Sharma <udaysharma.2310@gmail.com>
2025-06-26 19:03:40 +0530

    Added Anonymous Class example

---
+++ Classes and Object/Class.js/anynomousClass.js
@@ -0,0 +1,15 @@
+var a=class{
+constructor()
+{
+this.x=10;
+this.y=20;
+}
+}
+
+console.log(a);
+var b=new a();
+console.log(b);
+
+
+
+// var a=class abcd{}  - if here we gave the name of a class but still it is pointing to var a.
