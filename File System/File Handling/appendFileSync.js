b90b705c14b78fe79733ba190e8f0c8b3cce91de
Uday Sharma <udaysharma.2310@gmail.com>
2025-07-15 16:13:40 +0530

    Added appendFileSync.js

---
+++ File System/File Handling/appendFileSync.js
@@ -0,0 +1,4 @@
+const fs=require('fs');
+fs.appendFileSync("Whatever.d","God is Great\n","utf-8");
+fs.appendFileSync("Whatever.d","Ujjain is city of God\n","utf-8");
+fs.appendFileSync("Whatever.d","I live in ujjain\n","utf-8");
